ebf904ab31a30d30a56ccd80294e31bcf1dd05af
oscarbuse <oscar@kwalinux.nl>
2021-07-11 11:56:25 +0200

    added blueprints en templates

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 # db variable initialization
 db = SQLAlchemy()
@@ -13,6 +15,9 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
+
+    Bootstrap(app)
+
     db.init_app(app)
 
     login_manager.init_app(app)
@@ -20,11 +25,16 @@ def create_app():
     login_manager.login_view = "auth.login"
 
     migrate = Migrate(app, db)
+
     from app import models
 
-    # temporary route
-    @app.route('/')
-    def hello_world():
-      return 'Hello, World!'
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
     return app
--- run.py
+++ run.py
@@ -1,12 +1,8 @@
 # run.py
 
-import os
-
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG')
 app = create_app()
-
 
 if __name__ == '__main__':
     app.run()
