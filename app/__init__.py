01d876bdd6e1a878939e661300413a19d78e26d5
oscarbuse <oscar@kwalinux.nl>
2021-07-11 13:39:06 +0200

    removed roles en admin for now

--- app/__init__.py
+++ app/__init__.py
@@ -28,13 +28,10 @@ def create_app():
 
     from app import models
 
-    from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
-
-    from .home import home as home_blueprint
-    app.register_blueprint(home_blueprint)
 
     return app
--- app/models.py
+++ app/models.py
@@ -19,8 +19,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(60), index=True, unique=True)
     username = db.Column(db.String(60), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    is_admin = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -50,18 +48,3 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
-class Role(db.Model):
-    """
-    Create a Role table
-    """
-
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(60), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Role: {}>'.format(self.name)
